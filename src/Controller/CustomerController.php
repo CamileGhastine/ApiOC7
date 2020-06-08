7542df5b766bdb72a79a02c8be7e0c8be4926cc9
CamileGhastine <camileghastine@hotmail.com>
2020-06-08 20:29:18 +0100

    CustomerController index()

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Customer;
+use JMS\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -16,8 +19,21 @@ class CustomerController extends AbstractController
     /**
      * @Route("/customers", name="list_customer", methods={"GET"})
      */
-    public function index(Request $request)
+    public function index(SerializerInterface $serializer)
     {
+        $customer = new Customer();
+        $customer->setEmail('camile@camile.fr')
+            ->setFirstName('camile')
+            ->setLastName('ghastine')
+            ->setAdress('rue de bellevue')
+            ->setPostalCode(77176)
+            ->setCity('Paris');
+
+        $data = $serializer->serialize($customer, 'json');
+
+        return new Response($data, Response::HTTP_OK, [
+            'Content-Type' => 'application/json'
+        ]);
 
     }
 }
---
+++ src/Controller/PhoneController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Phone;
+use App\Repository\PhoneRepository;
+use App\Service\ParametersRepositoryPreparator;
+use Exception;
+use JMS\Serializer\SerializationContext;
+use JMS\Serializer\SerializerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class PhoneController
+ * @Route("/api")
+ * @package App\Controller
+ */
+class PhoneController extends AbstractController
+{
+    /**
+     * @Route("/phones", name="list_phone", methods={"GET"})
+     *
+     * @param Request $request
+     * @param PhoneRepository $phoneRepository
+     * @param SerializerInterface $serializer
+     * @param ParametersRepositoryPreparator $preparator
+     *
+     * @return Response
+     *
+     * @throws Exception
+     */
+    public function index(Request $request, PhoneRepository $phoneRepository, SerializerInterface $serializer, ParametersRepositoryPreparator $preparator)
+    {
+        $parameters = $preparator->prepareParametersPhone($request, $this->getParameter('paginator.maxResult'));
+
+        // if $parameters have message errors
+        if (isset($parameters['error'])) {
+            $data = [
+                'status' => Response::HTTP_BAD_REQUEST,
+                'message' => $parameters['error']
+            ];
+
+            return new JsonResponse($data, Response::HTTP_BAD_REQUEST);
+        }
+
+        $phone = $phoneRepository->findPhonePaginated($parameters);
+
+        $data = $serializer->serialize($phone->getIterator(), 'json', SerializationContext::create()->setGroups(['list']));
+
+        return new Response($data, Response::HTTP_OK, [
+            'Content-Type' => 'application/json'
+        ]);
+    }
+
+    /**
+     * @Route("/phones/{id<\d+>}", name="show_phone", methods={"GET"})
+     *
+     * @param Phone $phone
+     * @param SerializerInterface $serializer
+     *
+     * @return Response
+     */
+    public function show(Phone $phone, SerializerInterface $serializer)
+    {
+        $data = $serializer->serialize($phone, 'json', SerializationContext::create()->setGroups(['detail']));
+
+        return new Response($data, Response::HTTP_OK, [
+            'Content-Type' => 'application/json'
+        ]);
+    }
+}
