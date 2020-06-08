163f7f6987ca289513e9df020f77c1843107fa40
CamileGhastine <camileghastine@hotmail.com>
2020-06-08 12:27:21 +0100

    subscriber of kernel.exception

---
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+
+class ExceptionSubscriber implements EventSubscriberInterface
+{
+    public function onKernelException(ExceptionEvent $event)
+    {
+        $exception = $event->getThrowable();
+
+        $response = new Response($event->getThrowable()->getMessage(), $event->getThrowable()->getStatusCode(), [
+            'Content-Type' => 'application/json'
+        ]);
+
+        $event->setResponse($response);
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'kernel.exception' => 'onKernelException',
+        ];
+    }
+}
